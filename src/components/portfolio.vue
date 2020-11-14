80d381bd47de3da561341cd46fe05c4c34cd83b5
AnigalHub <anigal1997@gmail.com>
2020-11-14 23:46:02 +0300

    добавлен массив и цикл на вывод

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -2,61 +2,88 @@
     <div id="portfolio">
         <b-container>
             <h2>Портфолио</h2>
-            <b-row>
-                <b-col>
-                    <img src="../assets/images/41.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/42.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/43.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/44.jpg"/>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col>
-                    <img src="../assets/images/45.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/46.jpeg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/47.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/48.jpg"/>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col>
-                    <img src="../assets/images/49.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/50.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/51.jpg"/>
-                </b-col>
-                <b-col>
-                    <img src="../assets/images/52.jpg"/>
-                </b-col>
-            </b-row>
+            <div class="flex-container">
+                <div v-for="(work, index) in Portfolio" :key="index">
+                    <img :src="work.src" :alt="work.alt"/>
+                </div>
+            </div>
         </b-container>
     </div>
 </template>
 
 <script>
     export default {
-        name: "portfolio"
+        name: "portfolio",
+        data(){
+            return{
+                publicPath: process.env.BASE_URL,
+                Portfolio:[
+                    {
+                        src: "./images/41.jpg",
+                        alt: "Круглая_накладка_на_3_розетки",
+                    },
+                    {
+                        src: "./images/42.jpg",
+                        alt: "Квадратная_накладка_на_3_выключателя",
+                    },
+                    {
+                        src: "./images/43.jpg",
+                        alt: "Накладка_на_бревно",
+                    },
+                    {
+                        src: "./images/44.jpg",
+                        alt: "Двойная_накладка_на_бревно",
+                    },
+                    {
+                        src: "./images/45.jpg",
+                        alt: "Накладка_под_выключатель",
+                    },
+                    {
+                        src: "./images/46.jpeg",
+                        alt: "Накладка_со_скрытой_установкой",
+                    },
+                    {
+                        src: "./images/47.jpg",
+                        alt: "Ретро_накладка",
+                    },
+                    {
+                        src: "./images/48.jpg",
+                        alt: "Ретро_накладка_под_бревно",
+                    },
+                    {
+                        src: "./images/49.jpg",
+                        alt: "Накладка_под_разные_розетки",
+                    },
+                    {
+                        src: "./images/50.jpg",
+                        alt: "Накладка_под_бревно_на_выключатели",
+                    },
+                    {
+                        src: "./images/51.jpg",
+                        alt: "Накладка_из_темного_дуба_под_выключатели",
+                    },
+                    {
+                        src: "./images/52.jpg",
+                        alt: "Накладка_под_розетку",
+                    },
+                ],
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
+    .flex-container {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .flex-container > div {
+        background: radial-gradient(70% 70%, #ffffff, rgb(210, 206, 211));
+        width: 22%;
+        margin: 0 1.5% 2.2% 1.5% !important;
+    }
     #portfolio{
-        background: url("../../src/assets/images/background_2.jpg") 100% 100% no-repeat;
+        background: url("../../public/images/background_2.jpg") 100% 100% no-repeat;
         background-size:100% 100%;
     }
     img{
@@ -79,6 +106,14 @@
         }
         .col{
             padding: 0 10px !important;
+        }
+    }
+
+    @media screen and  (max-width: 768px) {
+        .flex-container > div {
+            background: radial-gradient(70% 70%, #ffffff, rgb(210, 206, 211));
+            width: 30%;
+            margin: 0 1.5% 2.2% 1.5% !important;
         }
     }
 </style>
