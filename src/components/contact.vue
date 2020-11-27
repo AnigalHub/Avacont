51183653abeae9fb30a762c3854438b1d024ebb7
AnigalHub <anigal1997@gmail.com>
2020-11-27 21:02:29 +0300

    добавлено почта, телефон, адреса и карта - чтение из json файлов

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -6,30 +6,25 @@
                 <b-col cols="5">
                     <div class="address text">
                         <div class="telephone">
-                            <u> Телефон: </u>
-                            <a :href="'tel:'+ phone"> {{phone}}</a>
+                            <u> Телефон: </u><a :href="'tel:'+ phone"> {{phone}}</a>
                         </div>
                         <div class="mail">
-                            <u>E-mail:</u>
-                            <a :href="'mailto:'+ mail">{{mail}}</a>
+                            <u>E-mail:</u><a :href="'mailto:'+ mail">{{mail}}</a>
                         </div>
                         <div>
-                            <u>Часы работы: </u><br>
-                            {{working_hours}}
+                            <u>Часы работы: </u><br>{{working_hours}}
                         </div>
                         <div>
-                            <u> Адрес производства: </u><br>
-                            {{manufacturers_address}}
+                            <u> Адрес производства: </u><br>{{manufacturers_address}}
                         </div>
                         <div>
-                            <u> Адрес офиса продаж: </u><br>
-                            {{sales_office_address}}
+                            <u> Адрес офиса продаж: </u><br>{{sales_office_address}}
                         </div>
                     </div>
                 </b-col>
                 <b-col>
                     <div class="feature map" id="map-wrap">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1337.5065661296035!2d37.90404039139417!3d55.68187746704363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab62f6d977c57%3A0xb6fd3dd42c79d783!2z0YPQuy4g0JjQvdC40YbQuNCw0YLQuNCy0L3QsNGPLCAzNNCQLCDQm9GO0LHQtdGA0YbRiywgTW9zY293IE9ibGFzdCwgMTQwMDAw!5e0!3m2!1sru!2sru!4v1595516566930!5m2!1sru!2sru" frameborder="0"  allowfullscreen="" aria-hidden="false" ></iframe>
+                        <iframe :src="map" ></iframe>
                     </div>
                 </b-col>
             </b-row>
@@ -41,6 +36,9 @@
     import manufacturers_address from '../../public/documents/manufacturers_address';
     import sales_office_address from '../../public/documents/sales_office_address.json';
     import phone from '../../public/documents/phone.json';
+    import working_hours from '../../public/documents/working_hours.json';
+    import mail from '../../public/documents/mail.json';
+    import map from '../../public/documents/map.json';
     export default {
         name: "contact",
         data(){
@@ -48,8 +46,9 @@
                 manufacturers_address: manufacturers_address,
                 sales_office_address:sales_office_address,
                 phone:phone,
-                working_hours:'Пн. – Пт.: с 9:00 до 18:00',
-                mail:'avacont@mail.ru',
+                working_hours:working_hours,
+                mail:mail,
+                map:map,
             }
         }
     }
