e8dbbf5c9b1b5a253cbe3020d6f463e8e20a7e28
AnigalHub <anigal1997@gmail.com>
2020-11-28 18:16:40 +0300

    svg на вывод дополнительной информации

---
+++ src/components/more_details_svg.vue
@@ -0,0 +1,74 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 172 172" style=" fill:#000000;">
+        <defs>
+            <linearGradient x1="61.8125" y1="59.125" x2="61.8125" y2="75.25" gradientUnits="userSpaceOnUse" id="color-1_43586_gr1">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="94.0625" y1="64.5" x2="94.0625" y2="69.875" gradientUnits="userSpaceOnUse" id="color-2_43586_gr2">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="61.8125" y1="80.625" x2="61.8125" y2="96.75" gradientUnits="userSpaceOnUse" id="color-3_43586_gr3">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="94.0625" y1="86" x2="94.0625" y2="91.375" gradientUnits="userSpaceOnUse" id="color-4_43586_gr4">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="61.8125" y1="102.125" x2="61.8125" y2="118.25" gradientUnits="userSpaceOnUse" id="color-5_43586_gr5">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="88.6875" y1="107.5" x2="88.6875" y2="112.875" gradientUnits="userSpaceOnUse" id="color-6_43586_gr6">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="123.625" y1="18.8125" x2="123.625" y2="45.6875" gradientUnits="userSpaceOnUse" id="color-7_43586_gr7">
+                <stop offset="0" stop-color="#796161"></stop>
+                <stop offset="1" stop-color="#383838"></stop>
+            </linearGradient>
+            <linearGradient x1="86" y1="17.01994" x2="86" y2="171.23138" gradientUnits="userSpaceOnUse" id="color-8_43586_gr8">
+                <stop offset="0" stop-color="#3b2f2f"></stop>
+                <stop offset="1" stop-color="#000000"></stop>
+            </linearGradient>
+            <linearGradient x1="137.0625" y1="17.01994" x2="137.0625" y2="171.23138" gradientUnits="userSpaceOnUse" id="color-9_43586_gr9">
+                <stop offset="0" stop-color="#3b2f2f"></stop>
+                <stop offset="1" stop-color="#000000"></stop>
+            </linearGradient>
+            <linearGradient x1="137.0625" y1="17.01994" x2="137.0625" y2="171.23138" gradientUnits="userSpaceOnUse" id="color-10_43586_gr10">
+                <stop offset="0" stop-color="#3b2f2f"></stop>
+                <stop offset="1" stop-color="#000000"></stop>
+            </linearGradient>
+        </defs>
+        <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+            <path d="M0,172v-172h172v172z" fill="none"></path>
+            <g>
+                <path d="M61.8125,59.125c-4.56875,0 -8.0625,3.49375 -8.0625,8.0625c0,4.56875 3.49375,8.0625 8.0625,8.0625c4.56875,0 8.0625,-3.49375 8.0625,-8.0625c0,-4.56875 -3.49375,-8.0625 -8.0625,-8.0625zM61.8125,69.875c-1.6125,0 -2.6875,-1.075 -2.6875,-2.6875c0,-1.6125 1.075,-2.6875 2.6875,-2.6875c1.6125,0 2.6875,1.075 2.6875,2.6875c0,1.6125 -1.075,2.6875 -2.6875,2.6875z" fill="url(#color-1_43586_gr1)"></path>
+                <path d="M75.25,64.5h37.625v5.375h-37.625z" fill="url(#color-2_43586_gr2)"></path>
+                <path d="M61.8125,80.625c-4.56875,0 -8.0625,3.49375 -8.0625,8.0625c0,4.56875 3.49375,8.0625 8.0625,8.0625c4.56875,0 8.0625,-3.49375 8.0625,-8.0625c0,-4.56875 -3.49375,-8.0625 -8.0625,-8.0625zM61.8125,91.375c-1.6125,0 -2.6875,-1.075 -2.6875,-2.6875c0,-1.6125 1.075,-2.6875 2.6875,-2.6875c1.6125,0 2.6875,1.075 2.6875,2.6875c0,1.6125 -1.075,2.6875 -2.6875,2.6875z" fill="url(#color-3_43586_gr3)"></path>
+                <path d="M75.25,86h37.625v5.375h-37.625z" fill="url(#color-4_43586_gr4)"></path>
+                <path d="M61.8125,102.125c-4.56875,0 -8.0625,3.49375 -8.0625,8.0625c0,4.56875 3.49375,8.0625 8.0625,8.0625c4.56875,0 8.0625,-3.49375 8.0625,-8.0625c0,-4.56875 -3.49375,-8.0625 -8.0625,-8.0625zM61.8125,112.875c-1.6125,0 -2.6875,-1.075 -2.6875,-2.6875c0,-1.6125 1.075,-2.6875 2.6875,-2.6875c1.6125,0 2.6875,1.075 2.6875,2.6875c0,1.6125 -1.075,2.6875 -2.6875,2.6875z" fill="url(#color-5_43586_gr5)"></path>
+                <path d="M75.25,107.5h26.875v5.375h-26.875z" fill="url(#color-6_43586_gr6)"></path>
+                <path d="M110.1875,18.8125v24.1875c0,1.6125 1.075,2.6875 2.6875,2.6875h24.1875z" fill="url(#color-7_43586_gr7)"></path>
+                <path d="M40.3125,150.5c-1.34375,0 -2.6875,-1.34375 -2.6875,-2.6875v-3.225c0.80625,0.26875 1.88125,0.5375 2.6875,0.5375h57.24375c-0.26875,-1.88125 -0.5375,-3.49375 -0.80625,-5.375h-56.4375c-1.34375,0 -2.6875,-1.34375 -2.6875,-2.6875v-112.875c0,-1.34375 1.34375,-2.6875 2.6875,-2.6875h67.1875v18.8125c0,4.56875 3.49375,8.0625 8.0625,8.0625h18.8125v48.375c0.80625,0 1.88125,0 2.6875,0c0.80625,0 1.88125,0 2.6875,0v-48.9125c0,-2.15 -0.80625,-4.3 -2.41875,-5.64375l-23.65,-23.65c-1.6125,-1.6125 -3.49375,-2.41875 -5.64375,-2.41875h-67.725c-4.56875,0 -8.0625,3.49375 -8.0625,8.0625v123.625c0,4.56875 3.49375,8.0625 8.0625,8.0625h61.00625c-0.80625,-1.6125 -1.6125,-3.49375 -2.41875,-5.375zM112.875,25.2625l17.7375,17.7375h-15.05c-1.34375,0 -2.6875,-1.34375 -2.6875,-2.6875z" fill="url(#color-8_43586_gr8)"></path>
+                <path d="M137.0625,102.125c-19.35,0 -34.9375,15.5875 -34.9375,34.9375c0,19.35 15.5875,34.9375 34.9375,34.9375c19.35,0 34.9375,-15.5875 34.9375,-34.9375c0,-19.35 -15.5875,-34.9375 -34.9375,-34.9375zM137.0625,166.625c-16.39375,0 -29.5625,-13.16875 -29.5625,-29.5625c0,-16.39375 13.16875,-29.5625 29.5625,-29.5625c16.39375,0 29.5625,13.16875 29.5625,29.5625c0,16.39375 -13.16875,29.5625 -29.5625,29.5625z" fill="url(#color-9_43586_gr9)"></path>
+                <path d="M139.75,120.9375h-5.375v13.4375h-13.4375v5.375h13.4375v13.4375h5.375v-13.4375h13.4375v-5.375h-13.4375z" fill="url(#color-10_43586_gr10)"></path>
+            </g>
+        </g>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "more_details_svg"
+    }
+</script>
+
+<style scoped>
+svg{
+    width: 34px !important;
+    height: 34px !important;
+}
+</style>
