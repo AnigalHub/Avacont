68c8c2e4865bb37b492fe8279d66f4d315980774
AnigalHub <anigal1997@gmail.com>
2021-04-10 23:38:22 +0300

    изменен размер шрифта на разных размерах экрана

--- src/components/myfooter.vue
+++ src/components/myfooter.vue
@@ -43,16 +43,16 @@
     }
     .col{
         cursor: default;
-        font-size: 1.15rem;
+        font-size: 1.25rem;
         text-align: center;
         color: rgba(255, 255, 255, 0.9);
         font-weight: 600;
-        font-family: "Tenor Sans", sans-serif;
         text-shadow: 1.2px 1.2px 1.2px #49240c;
     }
     @media screen and (max-width: 500px) {
         .col{
-            font-size:0.6rem;
+            font-size:0.8rem;
+            letter-spacing: -.8px;
             padding: 0 5px !important;
         }
         .col:last-child,.col:first-child{
@@ -61,7 +61,7 @@
     }
     @media screen and (min-width: 500px)and (max-width: 768px) {
         .col{
-            font-size:0.8rem;
+            font-size:1rem;
         }
         .col:last-child,.col:first-child{
             padding-top: 6%;
