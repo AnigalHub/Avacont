d9df883605c34423079c57258a8e8905bb3cea3b
AnigalHub <anigal1997@gmail.com>
2021-04-22 22:22:10 +0300

    добавлены комментарии

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <b-navbar id="Nav" class="navbar-expand-xl sticky-top" toggleable>
         <b-container>
-            <b-navbar-brand href=#home @click.prevent="scrollToTop()">
+            <b-navbar-brand href="#Home" @click.prevent="scrollToTop()">
                 <img src="../../public/favicon.jpg" alt="логотип_компании"/>
                 <div class="name_company">АВАКОНТ</div>
                 <div class="description_name">производственная компания</div>
@@ -14,16 +14,17 @@
             </b-navbar-toggle>
             <b-collapse id="navbar-toggle-collapse" class="justify-content-end" is-nav v-model="isExpanded">
                 <b-navbar-nav>
-                    <b-nav-item @click="scrollToId" href="#feature">Характеристики</b-nav-item>
-                    <b-nav-item @click="scrollToId" href="#catalog">Каталог</b-nav-item>
-                    <b-nav-item @click="scrollToId" href="#delivery">Доставка</b-nav-item>
-                    <b-nav-item @click="scrollToId" href="#portfolio">Наши работы</b-nav-item>
-                    <b-nav-item @click="scrollToId" href="#contact">Контакты</b-nav-item>
+                    <b-nav-item @click="scrollToId" href="#Features">Характеристики</b-nav-item>
+                    <b-nav-item @click="scrollToId" href="#Catalog">Каталог</b-nav-item>
+                    <b-nav-item @click="scrollToId" href="#Delivery">Доставка</b-nav-item>
+                    <b-nav-item @click="scrollToId" href="#Portfolio">Наши работы</b-nav-item>
+                    <b-nav-item @click="scrollToId" href="#Contact">Контакты</b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-container>
     </b-navbar>
 </template>
+
 
 <script>
     export default {
@@ -53,10 +54,12 @@
 </script>
 
 <style scoped lang="scss">
+    /*блок под логотип(картинка, название компании, что за компания(производственная))*/
     .navbar-brand{
         padding-top: 0.2rem !important;
         padding-bottom: 0.2rem !important;
     }
+    /*картинка в логотипе*/
     img{
         height: 40px !important;
         width: 40px !important;
@@ -66,33 +69,36 @@
         margin-right: 4px;
         border-radius: 7px;
     }
+    /*меню на всех экранах (которое будет и складываться на маленьких экранах и раскрываться на больших)*/
     .navbar{
         background: #b8a59c;
         background-size:cover;
         border-bottom: 5px #4E2B0B !important;
         padding: 0 !important;
     }
+    /* все кнопки в меню*/
     .nav-link{
         color: #4E2B0B !important;
         padding-right: 1rem !important;
         padding-left: 1rem !important;
         font-weight: 600;
         font-size: 1.3rem !important;
-
+        &:hover{ /*при наведении*/
+            font-weight: 700;
+            text-decoration: underline;
+            text-shadow: 0.2em 0.2em 0.3em #8f745c;
+        }
     }
-    .nav-link:hover{
-        font-weight: 700;
-        text-decoration: underline;
-        text-shadow: 0.2em 0.2em 0.3em #8f745c;
-    }
+    /*название компании "Аваконт" в логотипе*/
     .name_company{
         font-size: 1.4rem;
         color: #4E2B0B;
-        font-weight: 500;
+        font-weight: 700;
         font-style: italic;
-        letter-spacing: 4px;
+        letter-spacing: 9px;
         font-family: "Caveat", cursive;
     }
+    /*надпись "производственная компания" в логотипе*/
     .description_name{
         font-size: 0.65rem;
         margin-top: -6%;
@@ -101,6 +107,7 @@
         text-shadow: 0.025rem 0.025rem 0.025rem #4e2b0b;
     }
     @media screen and (max-width: 768px) {
+        /* все кнопки в меню*/
         .nav-link {
             font-size: 1rem !important;
             padding: 0.35rem 1rem !important;
