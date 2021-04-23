1f87ca093b386c1fce646d2644103d45d852e7be
AnigalHub <anigal1997@gmail.com>
2021-04-23 22:34:58 +0300

    изменены комментарии

--- src/components/myfooter.vue
+++ src/components/myfooter.vue
@@ -27,9 +27,11 @@
         background: #382b1f;
         background-size:100% 100%;
         padding: 0.8% 0;
-        a { /*ссылки*/
+        /*ссылки*/
+        a {
             text-shadow: 0.5px 0.5px 0.5px #7c644c;
             color: rgba(255, 255, 255, 0.9);
+            /*при наведении*/
             &:hover{
                 text-shadow: 0.5px 0.5px 0.5px #7c644c;
                 color: rgba(158, 113, 51, 0.53);
@@ -37,40 +39,48 @@
                 font-weight: 700;
             }
         }
-        .col{ /*параметры сетки бутстрап (блок под информацию)*/
+        /*параметры сетки бутстрап (блок под информацию)*/
+        .col{
             cursor: default;
             font-size: 1.25rem;
             text-align: center;
             color: rgba(255, 255, 255, 0.9);
             font-weight: 600;
             text-shadow: 1.2px 1.2px 1.2px #49240c;
-            &:last-child,&:first-child{padding-top: 2.2%;}/*блок с телефоном и блок с почтой*/
+            /*блок с телефоном и блок с почтой*/
+            &:last-child,&:first-child{padding-top: 2.2%;}
         }
     }
 
     @media screen and (max-width: 500px) {
         #Myfooter {
-            .col {  /*параметры сетки бутстрап (блок под информацию)*/
+            /*параметры сетки бутстрап (блок под информацию)*/
+            .col {
                 font-size: 0.8rem;
                 letter-spacing: -.8px;
                 padding: 0 5px !important;
-                &:last-child, &:first-child {padding-top: 8% !important;}/*блок с телефоном и блок с почтой*/
+                /*блок с телефоном и блок с почтой*/
+                &:last-child, &:first-child {padding-top: 8% !important;}
             }
         }
     }
     @media screen and (min-width: 500px)and (max-width: 768px) {
         #Myfooter {
-            .col {  /*параметры сетки бутстрап (блок под информацию)*/
+            /*параметры сетки бутстрап (блок под информацию)*/
+            .col {
                 font-size: 1rem;
-                &:last-child, &:first-child {padding-top: 6%;}/*блок с телефоном и блок с почтой*/
+                /*блок с телефоном и блок с почтой*/
+                &:last-child, &:first-child {padding-top: 6%;}
             }
         }
     }
     @media screen and (min-width: 768px)and (max-width: 992px) {
         #Myfooter {
-            .col {  /*параметры сетки бутстрап (блок под информацию)*/
+            /*параметры сетки бутстрап (блок под информацию)*/
+            .col {
                 font-size: 1rem;
-                &:last-child, &:first-child {padding-top: 4%;} /*блок с телефоном и блок с почтой*/
+                /*блок с телефоном и блок с почтой*/
+                &:last-child, &:first-child {padding-top: 4%;}
             }
         }
     }
